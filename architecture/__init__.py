e11deda06283d833dbcf57cad29957ae49d825aa
Gourav <gkshindia@gmail.com>
2026-01-05 09:48:44 +0530

    Add .gitignore, remove unused architecture module, and implement GPT model architecture

--- architecture/__init__.py
+++
@@ -1,1 +0,0 @@
-from .architecture import GPTModel
--- pretraining/architecture.py
+++ pretraining/architecture.py
@@ -1,6 +1,21 @@
 from typing import Any
 import torch
 import torch.nn as nn
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    vocab_size: int = 50257
+    context_length: int = 1024
+    em_dim: int = 768
+    n_heads: int = 12
+    n_layers: int = 12
+    drop_rate: float = 0.1
+    qkv_bias: bool = False
+
+
+GPT_CONFIG_124M = GPTConfig()
 
 
 class GELU(nn.Module):
